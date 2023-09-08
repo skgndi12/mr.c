bdca105a3fcf288022efdb744953338a09c67454
wnhlee <2wheeh@gmail.com>
2023-09-08 16:46:18 +0900

    feat: remove old ui

--- ui/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useTitle } from '../hooks/use-title';
-
-export default function Home() {
-  useTitle('Home');
-
-  return <section>This is Mr.C Home.</section>;
-}
