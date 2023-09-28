996505e5d1fb22cb9f8efad5227b13a122f1fb3d
wnhlee <2wheeh@gmail.com>
2023-09-28 18:13:55 +0900

    feat: set lint on common-components

--- ui/packages/common-components/.eslintrc.cjs
+++ ui/packages/common-components/.eslintrc.cjs
@@ -1,16 +1,27 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:storybook/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['tsconfig.json'],
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 };
+
+/**
+ * References
+ * https://typescript-eslint.io/linting/configs/#projects-with-type-checking
+ * https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended-type-checked.ts
+ * https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/stylistic-type-checked.ts
+ */
