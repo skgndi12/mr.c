0ff9ecb5670e9224df92028e259e59e2cb6170bb
skgndi12 <skgndi123@naver.com>
2023-11-06 14:43:42 +0900

    feat: update openapi spec

--- api/src/controller/http/dev/dev.v1.controller.ts
+++ api/src/controller/http/dev/dev.v1.controller.ts
@@ -1,20 +1,15 @@
 import { Request, Response, Router } from 'express';
-import express from 'express';
-import { Logger } from 'winston';
 
 import { GreetingV1Request } from '@controller/http/dev/request/dev.v1.request';
 import { GreetingV1Response } from '@controller/http/dev/response/dev.v1.response';
 import { methodNotAllowed } from '@controller/http/handler';
 
 export class DevV1Controller {
-  constructor(public logger: Logger) {}
-
   public routes = (): Router => {
     const router: Router = Router();
     const prefix = '/v1/dev';
 
     router
-      .use(express.json())
       .route(`${prefix}/greeting`)
       .post(this.greeting)
       .all(methodNotAllowed);
@@ -26,7 +21,6 @@ export class DevV1Controller {
     req: Request<any, any, GreetingV1Request>,
     res: Response<GreetingV1Response>
   ) => {
-    this.logger.info(req.body);
     res.send({ message: 'Hello World!' });
   };
 }
