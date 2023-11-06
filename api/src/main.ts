e0d412b289c1ee53d251d641c2694848f8dc9f8d
skgndi12 <skgndi123@naver.com>
2023-11-06 14:42:50 +0900

    feat: initialize repository and tx manager

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,13 @@
 import {
+  buildDatabaseConfig,
   buildHttpConfig,
   buildLoggerConfig,
   loadConfig
 } from '@src/config/loader';
+import { generatePrismaClient } from '@src/infrastructure/prisma/prisma.client';
+import { PrismaTransactionManager } from '@src/infrastructure/prisma/prisma.transaction.manager';
+import { PostgresqlProfileRepository } from '@src/infrastructure/repositories/postgresql/profile.repository';
+import { PostgresqlUserRepository } from '@src/infrastructure/repositories/postgresql/user.repository';
 import { initializeLogger } from '@src/logger/logger';
 
 import { HttpServer } from '@controller/http/server';
@@ -17,6 +22,12 @@ async function main() {
   }
 
   const logger = initializeLogger(buildLoggerConfig(config));
+  const prismaClient = generatePrismaClient(buildDatabaseConfig(config));
+  const prismaTransactionManager = new PrismaTransactionManager(prismaClient);
+
+  const profileRepository = new PostgresqlProfileRepository(prismaClient);
+  const userRepository = new PostgresqlUserRepository(prismaClient);
+
   const httpServer = new HttpServer(logger, buildHttpConfig(config));
   await httpServer.start();
   // await httpServer.close();
