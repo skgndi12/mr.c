592e0f28e9c3ca8e587aa9b190f02578a60359f6
skgndi12 <skgndi123@naver.com>
2023-12-14 14:40:34 +0900

    refactor: optimize error util function

--- api/src/util/error.ts
+++ api/src/util/error.ts
@@ -7,7 +7,7 @@ export function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
     typeof error === 'object' &&
     error !== null &&
     'message' in error &&
-    typeof (error as Record<string, unknown>).message === 'string'
+    typeof error.message === 'string'
   );
 }
 
