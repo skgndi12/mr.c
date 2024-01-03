cced6d416e756cf920e308b54ca05b46ff99ea34
skgndi12 <skgndi123@naver.com>
2024-01-03 15:03:19 +0900

    refactor: modify initiate Google sign in signature

--- api/test/core/services/auth/auth.service.test.ts
+++ api/test/core/services/auth/auth.service.test.ts
@@ -10,8 +10,9 @@ jest.mock('crypto');
 
 describe('Test auth service', () => {
   const oauthClientId = 'test_client_id';
-  const oauthRedirectUri = '127.0.0.1:0/api/v1/google/sign-in/token';
+  const oauthRedirectPath = '/api/v1/google/sign-in/token';
   const oauthAuthEndpoint = 'https://accounts.google.com/o/oauth2/auth';
+  const host = 'localhost:8080';
   const nonce = '2fc312e4-0c04-4f93-b0bf-c9e81f244814';
   const state = '8a5c8a05-0a20-49f9-92a5-a6f83d5617e9';
   const response_type = 'code';
@@ -19,6 +20,7 @@ describe('Test auth service', () => {
   const scope = 'openid email';
   const access_type = 'online';
   const prompt = 'consent select_account';
+  const baseUrl = `${protocol}://${host}`;
   const authConfig: AuthConfig = { oauthStateExpirationMinutes: 10 };
   let keyValueRepository: KeyValueRepository;
   let googleHandler: GoogleHandler;
@@ -32,32 +34,32 @@ describe('Test auth service', () => {
     googleHandler = {
       buildOidcRequest: jest.fn(() => {
         return encodeURI(
-          `${oauthAuthEndpoint}?client_id=${oauthClientId}&nonce=${nonce}&response_type=${response_type}&redirect_uri=${protocol}://${oauthRedirectUri}&scope=${scope}&state=${state}&access_type=${access_type}&prompt=${prompt}`
+          `${oauthAuthEndpoint}?client_id=${oauthClientId}&nonce=${nonce}&response_type=${response_type}&redirect_uri=${baseUrl}${oauthRedirectPath}&scope=${scope}&state=${state}&access_type=${access_type}&prompt=${prompt}`
         );
       })
     };
     jest.spyOn(crypto, 'randomUUID').mockReturnValue(state);
+  });
+
+  it('should success initiate Google sign in and return valid OIDC request', async () => {
     authService = new AuthService(
       authConfig,
       keyValueRepository,
       googleHandler
     );
-  });
-
-  it('should return valid OIDC request', () => {
     const givenReferrer = 'http://127.0.0.1/home';
     const expectedQueyObject = {
       client_id: oauthClientId,
       response_type,
-      redirect_uri: `${protocol}://${oauthRedirectUri}`,
+      redirect_uri: `${baseUrl}${oauthRedirectPath}`,
       state,
       scope,
       access_type,
       prompt
     };
 
-    const redirectUrl = authService.initiateGoogleSignIn(
-      protocol,
+    const redirectUrl = await authService.initiateGoogleSignIn(
+      baseUrl,
       givenReferrer
     );
     const [authEndpoint, queryString] = redirectUrl.split('?');
@@ -79,6 +81,6 @@ describe('Test auth service', () => {
     );
 
     expect(googleHandler.buildOidcRequest).toBeCalledTimes(1);
-    expect(googleHandler.buildOidcRequest).toBeCalledWith(protocol, state);
+    expect(googleHandler.buildOidcRequest).toBeCalledWith(baseUrl, state);
   });
 });
