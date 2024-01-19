1e5f268af24a33bda6510542a436ec71c3bbf10b
skgndi12 <skgndi123@naver.com>
2024-01-19 17:18:56 +0900

    feat:  add signatures for user repository methods

--- api/src/core/ports/user.repository.ts
+++ api/src/core/ports/user.repository.ts
@@ -2,6 +2,8 @@ import { User } from '@src/core/entities/user.entity';
 import { TransactionClient } from '@src/core/ports/transaction.manager';
 
 export interface UserRepository {
+  findById(id: string, txClient?: TransactionClient): Promise<User>;
   findByEmail(email: string, txClient?: TransactionClient): Promise<User>;
   upsert(userData: User, txClient?: TransactionClient): Promise<User>;
+  deleteById(id: string, txClient?: TransactionClient): Promise<void>;
 }
