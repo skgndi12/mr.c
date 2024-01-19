ac520c42c506241a8dc480a77857c4a5b1e20da4
skgndi12 <skgndi123@naver.com>
2024-01-19 17:22:05 +0900

    feat: implement update user service method

--- api/src/core/services/user/user.service.ts
+++ api/src/core/services/user/user.service.ts
@@ -2,12 +2,20 @@ import { AccessLevel } from '@prisma/client';
 
 import { AppIdToken } from '@src/core/entities/auth.entity';
 import { User } from '@src/core/entities/user.entity';
+import {
+  TransactionClient,
+  TransactionManager
+} from '@src/core/ports/transaction.manager';
 import { UserRepository } from '@src/core/ports/user.repository';
 import { AccessLevelEnum } from '@src/core/types';
 import { AppErrorCode, CustomError } from '@src/error/errors';
+import { IsolationLevel } from '@src/infrastructure/repositories/types';
 
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly txManager: TransactionManager
+  ) {}
 
   public getUser = async (
     requesterIdToken: AppIdToken,
@@ -27,5 +35,36 @@ export class UserService {
     }
 
     return await this.userRepository.findById(requestedUserId);
+  };
+
+  public updateUser = async (
+    requesterIdToken: AppIdToken,
+    requestedUserId: string,
+    requestedAccessLevel: AccessLevel
+  ): Promise<User> => {
+    if (
+      !requesterIdToken.isAccessLevelAuthorized(
+        new AccessLevelEnum(AccessLevel.ADMIN)
+      )
+    ) {
+      throw new CustomError({
+        code: AppErrorCode.PERMISSIION_DENIED,
+        message: 'insufficient access level to update user',
+        context: { accessLevel: requesterIdToken.accessLevel }
+      });
+    }
+
+    return (await this.txManager.runInTransaction(
+      async (txClient: TransactionClient): Promise<User> => {
+        const userFound = await this.userRepository.findById(
+          requestedUserId,
+          txClient
+        );
+        userFound.accessLevel = new AccessLevelEnum(requestedAccessLevel);
+
+        return await this.userRepository.upsert(userFound, txClient);
+      },
+      IsolationLevel.READ_COMMITTED
+    )) as User;
   };
 }
--- api/test/core/services/user/user.service.test.ts
+++ api/test/core/services/user/user.service.test.ts
@@ -2,6 +2,7 @@ import { AccessLevel, Idp } from '@prisma/client';
 
 import { AppIdToken } from '@src/core/entities/auth.entity';
 import { User } from '@src/core/entities/user.entity';
+import { TransactionManager } from '@src/core/ports/transaction.manager';
 import { UserRepository } from '@src/core/ports/user.repository';
 import { UserService } from '@src/core/services/user/user.service';
 import { AccessLevelEnum, IdpEnum } from '@src/core/types';
@@ -22,6 +23,7 @@ describe('Test user service', () => {
     updatedAt: currentDate
   };
   let userRepository: UserRepository;
+  let txManager: TransactionManager;
 
   beforeAll(() => {
     userRepository = {
@@ -29,6 +31,9 @@ describe('Test user service', () => {
       findByEmail: jest.fn(),
       upsert: jest.fn(),
       deleteById: jest.fn()
+    };
+    txManager = {
+      runInTransaction: jest.fn()
     };
   });
 
@@ -50,10 +55,10 @@ describe('Test user service', () => {
         'user1@gmail.com',
         new AccessLevelEnum(AccessLevel.DEVELOPER)
       );
-      const actualResult = await new UserService(userRepository).getUser(
-        givenRequesterIdToken,
-        requestedUserId
-      );
+      const actualResult = await new UserService(
+        userRepository,
+        txManager
+      ).getUser(givenRequesterIdToken, requestedUserId);
 
       expect(actualResult).toStrictEqual(user);
 
@@ -71,7 +76,7 @@ describe('Test user service', () => {
           'user1@gmail.com',
           new AccessLevelEnum(AccessLevel.USER)
         );
-        await new UserService(userRepository).getUser(
+        await new UserService(userRepository, txManager).getUser(
           givenRequesterIdToken,
           requestedUserId
         );
@@ -81,6 +86,71 @@ describe('Test user service', () => {
       }
 
       expect(userRepository.findById).toBeCalledTimes(0);
+    });
+  });
+
+  describe('Test update user', () => {
+    const givenRequesterIdToken = new AppIdToken(
+      requesterUserId,
+      'nickname',
+      '#TAGG',
+      new IdpEnum(Idp.GOOGLE),
+      'user1@gmail.com',
+      new AccessLevelEnum(AccessLevel.ADMIN)
+    );
+
+    const upsertedUser: User = {
+      id: user.id,
+      nickname: user.nickname,
+      tag: user.tag,
+      idp: user.idp,
+      email: user.email,
+      accessLevel: new AccessLevelEnum(AccessLevel.ADMIN),
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
+    };
+    const requestedAccessLevel = AccessLevel.DEVELOPER;
+
+    beforeAll(() => {
+      txManager.runInTransaction<User> = jest.fn(() => Promise.resolve(upsertedUser))
+    });
+
+    it('should success when valid', async () => {
+      const actualResult = await new UserService(
+        userRepository,
+        txManager
+      ).updateUser(
+        givenRequesterIdToken,
+        requestedUserId,
+        requestedAccessLevel
+      );
+
+      expect(actualResult).toStrictEqual(upsertedUser);
+
+      expect(txManager.runInTransaction).toBeCalledTimes(1);
+    });
+
+    it("should failure when requester's access level is not sufficient", async () => {
+      try {
+        const givenRequesterIdToken = new AppIdToken(
+          requesterUserId,
+          'nickname',
+          '#TAGG',
+          new IdpEnum(Idp.GOOGLE),
+          'user1@gmail.com',
+          new AccessLevelEnum(AccessLevel.USER)
+        );
+        await new UserService(userRepository, txManager).updateUser(
+          givenRequesterIdToken,
+          requestedUserId,
+          requestedAccessLevel
+        );
+      } catch (error: unknown) {
+        expect(error).toBeInstanceOf(CustomError);
+        expect(error).toHaveProperty('code', AppErrorCode.PERMISSIION_DENIED);
+      }
+
+      expect(txManager.runInTransaction).toBeCalledTimes(0);
     });
   });
 });
