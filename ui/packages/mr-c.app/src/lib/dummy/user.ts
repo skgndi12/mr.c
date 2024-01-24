40f28377d69c833b0346e9d7506620b70f0182e5
wnhlee <2wheeh@gmail.com>
2024-01-24 23:55:52 +0900

    feat: add dummy data

---
+++ ui/packages/mr-c.app/src/lib/dummy/user.ts
@@ -0,0 +1,13 @@
+// TODO: Remove dummy after connected with backend apis
+
+import type { GetUserDetailResponse } from '@/lib/definitions/user';
+
+const dummyUserDetailResponse: GetUserDetailResponse = {
+  id: '7289ae9c-42b1-49ca-8f5b-a1831583e903',
+  nickname: '신비로운 평론가 붉은 여우',
+  tag: '#MQ3B',
+  idp: 'google',
+  accessLevel: 'USER',
+};
+
+export default dummyUserDetailResponse;
