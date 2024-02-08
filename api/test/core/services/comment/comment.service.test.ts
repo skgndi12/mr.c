6121280995a9c07ee92266e54e829aa9233d58ec
skgndi12 <skgndi123@naver.com>
2024-02-08 14:05:58 +0900

    feat: implement get comments service method

--- api/test/core/services/comment/comment.service.test.ts
+++ api/test/core/services/comment/comment.service.test.ts
@@ -10,8 +10,12 @@ import { CommentRepository } from '@src/core/ports/comment.repository';
 import { TransactionManager } from '@src/core/ports/transaction.manager';
 import { UserRepository } from '@src/core/ports/user.repository';
 import { CommentService } from '@src/core/services/comment/comment.service';
-import { CreateCommentDto } from '@src/core/services/comment/types';
+import {
+  CreateCommentDto,
+  GetCommentsDto
+} from '@src/core/services/comment/types';
 import { AccessLevelEnum, IdpEnum } from '@src/core/types';
+import { AppErrorCode, CustomError } from '@src/error/errors';
 import { PrismaTransactionManager } from '@src/infrastructure/prisma/prisma.transaction.manager';
 import { ExtendedPrismaClient } from '@src/infrastructure/prisma/types';
 import { PostgresqlCommentRepository } from '@src/infrastructure/repositories/postgresql/comment.repository';
@@ -23,6 +27,15 @@ jest.mock('@root/test/infrastructure/prisma/test.prisma.client', () => ({
 }));
 
 const prismaMock = extendedPrisma as DeepMockProxy<ExtendedPrismaClient>;
+
+function calculateCommentTotalPageCount(
+  commentCount: number,
+  pageSize?: number
+) {
+  const givenPageSize = pageSize ?? 10;
+  const additionalPageCount = commentCount % givenPageSize !== 0 ? 1 : 0;
+  return Math.floor(commentCount / givenPageSize) + additionalPageCount;
+}
 
 describe('Test comment service', () => {
   let userRepository: UserRepository;
@@ -140,6 +153,165 @@ describe('Test comment service', () => {
           content: givenDto.content
         })
       );
+    });
+  });
+
+  describe('Test get comments', () => {
+    const userId = 'randomId';
+    const nickname = 'randomNickname';
+    const tag = '#TAGG';
+    const idp = new IdpEnum(Idp.GOOGLE);
+    const email = 'user1@gmail.com';
+    const accessLevel = new AccessLevelEnum(AccessLevel.USER);
+    const movieName = 'randomMovie';
+    const content = 'randomContent';
+    const createdAt = new Date();
+    const users: User[] = [];
+    const comments: Comment[] = [];
+    const commentCount = 10;
+
+    const userFindByIds = jest.fn(() => Promise.resolve(users)) as jest.Mock;
+    const commentFinManyAndCount = jest.fn(() =>
+      Promise.resolve({ comments, commentCount })
+    ) as jest.Mock;
+
+    beforeAll(() => {
+      users.push(
+        new User(
+          userId,
+          nickname,
+          tag,
+          idp,
+          email,
+          accessLevel,
+          createdAt,
+          createdAt
+        )
+      );
+
+      for (let i = 1; i <= commentCount; i++) {
+        comments.push(
+          new Comment(i, userId, movieName, content, createdAt, createdAt)
+        );
+      }
+
+      prismaMock.$transaction.mockImplementation((callback) =>
+        callback(prismaMock)
+      );
+      userRepository = new PostgresqlUserRepository(prismaMock);
+      commentRepository = new PostgresqlCommentRepository(prismaMock);
+      txManager = new PrismaTransactionManager(prismaMock);
+      userRepository.findByIds = userFindByIds;
+      commentRepository.findManyAndCount = commentFinManyAndCount;
+    });
+
+    it('should success when page size is not provided', async () => {
+      const givenDto: GetCommentsDto = {
+        nickname,
+        movieName
+      };
+      const actualResults = await new CommentService(
+        userRepository,
+        commentRepository,
+        txManager
+      ).getComments(givenDto);
+      const totalPageCount = calculateCommentTotalPageCount(commentCount);
+
+      expect(actualResults.totalPageCount).toEqual(totalPageCount);
+      expect(JSON.stringify(actualResults.users)).toEqual(
+        JSON.stringify(users)
+      );
+      for (let i = 0; i <= 10; i++) {
+        expect(JSON.stringify(actualResults.comments[i])).toEqual(
+          JSON.stringify(comments[i])
+        );
+      }
+
+      expect(userRepository.findByIds).toBeCalledTimes(1);
+      const userFindByIdArgs = userFindByIds.mock.calls[0][0];
+      expect(userFindByIdArgs).toEqual([userId]);
+
+      expect(commentRepository.findManyAndCount).toBeCalledTimes(1);
+      const commentFindManyAndCountArgs =
+        commentFinManyAndCount.mock.calls[0][0];
+      expect(commentFindManyAndCountArgs).toEqual(
+        expect.objectContaining({
+          nickname: givenDto.nickname,
+          movieName: givenDto.movieName,
+          sortBy: 'createdAt',
+          direction: 'desc',
+          pageOffset: 1,
+          pageSize: 10
+        })
+      );
+    });
+
+    it('should success when a page size is in the range of 1 to 100', async () => {
+      const givenPageSize = 5;
+      const givenDto: GetCommentsDto = {
+        nickname,
+        movieName,
+        pageSize: givenPageSize
+      };
+      const actualResults = await new CommentService(
+        userRepository,
+        commentRepository,
+        txManager
+      ).getComments(givenDto);
+      const totalPageCount = calculateCommentTotalPageCount(
+        commentCount,
+        givenPageSize
+      );
+
+      expect(actualResults.totalPageCount).toEqual(totalPageCount);
+      expect(JSON.stringify(actualResults.users)).toEqual(
+        JSON.stringify(users)
+      );
+      for (let i = 0; i <= givenPageSize; i++) {
+        expect(JSON.stringify(actualResults.comments[i])).toEqual(
+          JSON.stringify(comments[i])
+        );
+      }
+
+      expect(userRepository.findByIds).toBeCalledTimes(1);
+      const userFindByIdArgs = userFindByIds.mock.calls[0][0];
+      expect(userFindByIdArgs).toEqual([userId]);
+
+      expect(commentRepository.findManyAndCount).toBeCalledTimes(1);
+      const commentFindManyAndCountArgs =
+        commentFinManyAndCount.mock.calls[0][0];
+      expect(commentFindManyAndCountArgs).toEqual(
+        expect.objectContaining({
+          nickname: givenDto.nickname,
+          movieName: givenDto.movieName,
+          sortBy: 'createdAt',
+          direction: 'desc',
+          pageOffset: 1,
+          pageSize: givenPageSize
+        })
+      );
+    });
+
+    it('should fail when page size exceeds 100', async () => {
+      const givenPageSize = 101;
+      const givenDto: GetCommentsDto = {
+        nickname,
+        movieName,
+        pageSize: givenPageSize
+      };
+      try {
+        await new CommentService(
+          userRepository,
+          commentRepository,
+          txManager
+        ).getComments(givenDto);
+      } catch (error: unknown) {
+        expect(error).toBeInstanceOf(CustomError);
+        expect(error).toHaveProperty('code', AppErrorCode.BAD_REQUEST);
+      }
+
+      expect(userRepository.findByIds).toBeCalledTimes(0);
+      expect(commentRepository.findManyAndCount).toBeCalledTimes(0);
     });
   });
 });
