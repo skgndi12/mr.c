e18a9eb23617828ac870baef0e8d4e34c35ebbaf
skgndi12 <skgndi123@naver.com>
2024-02-08 19:41:20 +0900

    feat: implement create reply repository method

---
+++ api/src/infrastructure/repositories/postgresql/reply.repository.ts
@@ -0,0 +1,42 @@
+import { Reply } from '@src/core/entities/review.entity';
+import {
+  CreateReplyParams,
+  ReplyRepository
+} from '@src/core/ports/reply.repository';
+import { AppErrorCode, CustomError } from '@src/error/errors';
+import {
+  ExtendedPrismaClient,
+  ExtendedPrismaTransactionClient
+} from '@src/infrastructure/prisma/types';
+
+export class PostgresqlReplyRepository implements Partial<ReplyRepository> {
+  constructor(private readonly client: ExtendedPrismaClient) {}
+
+  public create = async (
+    params: CreateReplyParams,
+    txClient?: ExtendedPrismaTransactionClient
+  ): Promise<Reply> => {
+    const createdAt = new Date();
+    try {
+      const client = txClient ?? this.client;
+      const reply = await client.reply.create({
+        data: {
+          reviewId: params.reviewId,
+          userId: params.userId,
+          content: params.content,
+          createdAt,
+          updatedAt: createdAt
+        }
+      });
+
+      return reply.convertToEntity();
+    } catch (error: unknown) {
+      throw new CustomError({
+        code: AppErrorCode.INTERNAL_ERROR,
+        cause: error,
+        message: 'failed to create reply',
+        context: { params }
+      });
+    }
+  };
+}
