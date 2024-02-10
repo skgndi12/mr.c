a8a0920cc01b38e1bd3ad97f0d2a46f4a4a735be
wnhlee <2wheeh@gmail.com>
2024-02-11 01:28:13 +0900

    feat: set proxy for the fetch url

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -3,6 +3,8 @@ import bundleAnalzer from '@next/bundle-analyzer';
 const withBundleAnalzyer = bundleAnalzer({ enabled: process.env.ANALYZE === 'true' });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: () => [{ source: '/api/:path*', destination: 'http://localhost:8080/api/:path*' }],
+};
 
 export default withBundleAnalzyer(nextConfig);
