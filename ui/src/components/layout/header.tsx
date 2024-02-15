af93198b4dd311bd69a5c15d5e9e92411b7220e7
wnhlee <2wheeh@gmail.com>
2024-02-15 11:27:51 +0900

    feat: make sign button to work fully on client side

--- ui/src/components/layout/header.tsx
+++ ui/src/components/layout/header.tsx
@@ -2,12 +2,9 @@ import { NavLinks } from '@/components/layout/nav-links';
 import Logo from '@/components/logo';
 import SideBar from '@/components/layout/sidebar';
 import Link from 'next/link';
-import { SignButton } from '@/components/layout/sign-button';
-import { cookies } from 'next/headers';
+import SignButton from '@/components/layout/sign-button';
 
 export function Header() {
-  const isLoggedIn = !!cookies().get('mrcToken');
-
   return (
     <header className="z-20 flex w-full items-center gap-4 border-b bg-white px-6 py-4">
       <div className="block">
@@ -19,9 +16,9 @@ export function Header() {
         <nav className="hidden items-center gap-4 sm:flex">
           <NavLinks />
         </nav>
-        <SignButton isLoggedIn={isLoggedIn} />
+        <SignButton />
         <nav className="flex items-center sm:hidden">
-          <SideBar SignButton={<SignButton isLoggedIn={isLoggedIn} />} />
+          <SideBar SignButton={<SignButton />} />
         </nav>
       </div>
     </header>
--- ui/src/components/layout/sign-button.tsx
+++ ui/src/components/layout/sign-button.tsx
@@ -1,24 +1,17 @@
 'use client';
 
 import Text from '@/components/atomic/text';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { useAuth } from '@/context/auth-context';
 
-export function SignButton({ isLoggedIn }: { isLoggedIn: boolean }) {
-  const router = useRouter();
+export default function SignButton() {
+  const { user, signOut, signIn } = useAuth();
 
-  if (isLoggedIn) {
+  if (user) {
     return (
       <button
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         onClick={async () => {
-          const res = await fetch('/api/v1/auth/sign-out');
-
-          if (res.ok) {
-            router.refresh();
-          } else {
-            // TODO: handle error - emit error toast ?
-          }
+          await signOut();
         }}
       >
         <Text size="lg" weight="medium">
@@ -29,10 +22,10 @@ export function SignButton({ isLoggedIn }: { isLoggedIn: boolean }) {
   }
 
   return (
-    <Link className="hover:cursor-pointer" href="/api/v1/google/sign-in">
+    <button onClick={() => signIn()}>
       <Text size="lg" weight="medium">
         Sign In
       </Text>
-    </Link>
+    </button>
   );
 }
