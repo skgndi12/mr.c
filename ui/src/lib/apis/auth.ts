ebe29da6c4f34ce9deb14cffab18ae0afdf98d0a
wnhlee <2wheeh@gmail.com>
2024-02-15 11:22:55 +0900

    feat: add lib for backend apis

---
+++ ui/src/lib/apis/auth.ts
@@ -0,0 +1,25 @@
+import { User } from '@/lib/definitions/user';
+
+export async function fetchUser() {
+  const response = await fetch('/api/v1/users/self');
+
+  if (!response.ok) {
+    return;
+  }
+
+  const data = (await response.json()) as { user: User };
+  return data.user;
+}
+
+export async function fetchSignOut() {
+  const response = await fetch('/api/v1/auth/sign-out');
+
+  if (!response.ok) {
+    // TODO: handle error
+    throw new Error('fail to sign out');
+  }
+}
+
+export function linkToSignIn() {
+  window.location.href = '/api/v1/google/sign-in';
+}
