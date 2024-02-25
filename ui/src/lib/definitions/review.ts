ab4211337e5489d1efad5a53cad7a6a70ad14f1e
wnhlee <2wheeh@gmail.com>
2024-02-25 22:12:46 +0900

    feat: update Review definitions and dummy

--- ui/src/lib/definitions/review.ts
+++ ui/src/lib/definitions/review.ts
@@ -1,6 +1,6 @@
 import type { Pagination } from '@/lib/definitions/pagination';
 
-interface Review {
+export interface Review {
   id: number; // 213,
   userId: string; // "{UUID v4}",
   nickname: string; // "신비로운 평론가 붉은 여우",
@@ -12,14 +12,14 @@ interface Review {
   updatedAt: string; //"2023-04-02T15:08:00+09:00"
 }
 
-export type GetReviewDetailResponse = Review;
+export interface ReviewFilter {
+  nickname?: string;
+  title?: string;
+  movieName?: string;
+}
 
 export interface ListReviewsResponse {
   reviews: Review[];
   pagination: Pagination;
-  filter?: {
-    nickname?: string;
-    title?: string;
-    movieName?: string;
-  };
+  filter?: ReviewFilter;
 }
