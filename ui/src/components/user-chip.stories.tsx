24a6316c9249105e499683d22f0aa98a59c6c60a
wnhlee <2wheeh@gmail.com>
2024-02-25 16:53:05 +0900

    feat: improve UserChip dropdowns

--- ui/src/components/user-chip.stories.tsx
+++ ui/src/components/user-chip.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent, expect } from '@storybook/test';
 
 import UserChip from '@/components/user-chip';
+import { action } from '@storybook/addon-actions';
 
 const meta = {
   title: 'Common/UserChip',
@@ -16,6 +17,22 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        push(...args: string[]) {
+          alert(`router pushes to: ${args[0]}`);
+
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          action('nextNavigation.push')(...args);
+
+          return Promise.resolve(true);
+        },
+      },
+    },
+  },
+
   args: {
     nickname: '신비로운 평론가 붉은 여우',
     tag: '#MQ3B',
@@ -23,7 +40,7 @@ export const Default: Story = {
 
   decorators: [
     (Story) => (
-      <div className="flex items-center justify-center p-10">
+      <div className="flex items-center justify-center p-14">
         <div className="fixed inset-0 bg-emerald-100 text-center hover:bg-emerald-200">outside</div>
         <Story />
       </div>
@@ -33,7 +50,8 @@ export const Default: Story = {
   play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement);
     const element = canvas.getByTestId('user-chip');
-    let more: HTMLElement;
+    let moreReview: HTMLElement;
+    let moreComment: HTMLElement;
     let outside: HTMLElement;
 
     await expect(element).toBeInTheDocument();
@@ -41,15 +59,24 @@ export const Default: Story = {
     await step('should open dropdown on click element', async () => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       await userEvent.click(element);
-      more = canvas.getByText('작성글 보기');
-      await expect(more).toBeInTheDocument();
+
+      moreReview = canvas.getByText('작성 리뷰');
+      await expect(moreReview).toBeInTheDocument();
+
+      moreComment = canvas.getByText('작성 코멘트');
+      await expect(moreComment).toBeInTheDocument();
     });
 
-    await step('shoulc close dropdown on click outside', async () => {
+    // TODO: test actions programatically - 현재 storybook의 first class 지원은 없음
+    // https://stackoverflow.com/questions/77240899/how-can-i-assert-that-a-storybook-action-was-fired
+
+    await step('should close dropdown on click outside', async () => {
       outside = canvas.getByText('outside');
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       await userEvent.click(outside);
-      await expect(more).not.toBeInTheDocument();
+
+      await expect(moreReview).not.toBeInTheDocument();
+      await expect(moreComment).not.toBeInTheDocument();
     });
   },
 };
