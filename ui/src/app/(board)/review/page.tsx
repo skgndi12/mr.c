f374ac980fac6457007d8de37107278475d646ea
wnhlee <2wheeh@gmail.com>
2024-02-27 11:05:08 +0900

    feat: implement review board page

--- ui/src/app/(board)/review/page.tsx
+++ ui/src/app/(board)/review/page.tsx
@@ -1,34 +1,33 @@
-import Link from 'next/link';
+import { Suspense } from 'react';
 
-export default function Page() {
+import { ReviewBoardHeader } from '@/components/review-board-header';
+import { ReviewCardsList } from '@/components/review-cards-list';
+
+import type { ListReviewsQuery } from '@/lib/definitions/review';
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: Omit<ListReviewsQuery, 'pageOffset' | 'pageSize'> & {
+    page?: string;
+  };
+}) {
+  const query: ListReviewsQuery = {
+    movieName: searchParams?.movieName,
+    nickname: searchParams?.nickname,
+    title: searchParams?.title,
+    pageOffset: Number(searchParams?.page),
+  };
+
   return (
-    <>
-      <h1>This is review board</h1>
-      <Link href="/review/1">review #1 detail</Link>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-      <h1>This is review board</h1>
-    </>
+    <main className="container mx-auto flex max-w-5xl flex-col items-center">
+      <ReviewBoardHeader />
+
+      <section className="w-full py-4">
+        <Suspense key={JSON.stringify(query)} /* TODO: fallback skeleton */>
+          <ReviewCardsList query={query} />
+        </Suspense>
+      </section>
+    </main>
   );
 }
