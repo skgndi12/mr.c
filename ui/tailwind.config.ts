b617c122d9a640c0b81e76dbaab7bac43368841c
wnhlee <2wheeh@gmail.com>
2024-03-10 18:54:25 +0900

    feat: implement Toast

--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -4,11 +4,18 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      animation: {
+        'slide-up': 'slide-up 0.3s ease-in-out both',
+      },
       keyframes: {
         shimmer: {
           '100%': {
             transform: 'translateX(100%)',
           },
+        },
+        'slide-up': {
+          '0%': { transform: 'translateY(30%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
         },
       },
     },
