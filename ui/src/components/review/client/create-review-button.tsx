9e0166b9e5f90a8e7298ee87beb68c6aeedddc8e
wnhlee <2wheeh@gmail.com>
2024-03-11 23:31:05 +0900

    fixup: CreateReviewButton

--- ui/src/components/review/client/create-review-button.tsx
+++ ui/src/components/review/client/create-review-button.tsx
@@ -20,56 +20,80 @@ export function CreateReviewButton() {
   const { title, movieName, movieNameRef, titleRef, disabled, setDisabled } = useReview();
   const { editorRef } = useEditorRef() ?? {};
 
-  const handleClick = () => {
-    setDisabled(true);
+  const validateAndGetData = () => {
+    if (!editorRef?.current) {
+      return;
+    }
 
-    const editorState = editorRef?.current?.getEditorState();
+    const editorState = editorRef.current.getEditorState();
 
-    if (editorState) {
-      const validatedFields = validateReviewFields({ title, movieName, editorState });
+    const validatedFields = validateReviewFields({ title, movieName, editorState });
 
-      if (!validatedFields.success) {
-        // Order to check should be aligned with the order of elements in the DOM
-        // title -> movieName -> editor
-        if (validatedFields.errors.title) {
-          titleRef.current?.focus();
-          emitToast(validatedFields.errors.title, 'error');
-        } else if (validatedFields.errors.movieName) {
-          movieNameRef.current?.focus();
-          emitToast(validatedFields.errors.movieName, 'error');
-        } else {
-          editorRef?.current?.focus();
-          emitToast(validatedFields.errors.content!, 'error');
-        }
-
-        setDisabled(false);
-        return;
+    if (!validatedFields.success) {
+      // Order to check should be aligned with the order of elements in the DOM
+      // title -> movieName -> editor
+      if (validatedFields.errors.title) {
+        titleRef.current?.focus();
+        emitToast(validatedFields.errors.title, 'error');
+      } else if (validatedFields.errors.movieName) {
+        movieNameRef.current?.focus();
+        emitToast(validatedFields.errors.movieName, 'error');
+      } else {
+        editorRef?.current?.focus();
+        emitToast(validatedFields.errors.content!, 'error');
       }
 
-      void createReview(validatedFields.data)
-        .then(({ review }) => {
-          emitToast('리뷰 등록 완료', 'success');
-          router.push(`/review/${review.id}`);
-        })
-        .catch((error) => {
-          if (isErrorWithMessage(error)) {
-            emitToast(error.message, 'error');
-          } else {
-            // TODO: throw new Error and move to global error handler
-            console.error(error);
-            emitToast('알 수 없는 에러가 발생했습니다. 다시 시도해주세요.', 'error');
-          }
-        })
-        .finally(() => setDisabled(false));
+      setDisabled(false);
+      return;
+    }
+
+    return validatedFields.data;
+  };
+
+  const handleApiError = (error: unknown) => {
+    if (isErrorWithMessage(error)) {
+      emitToast(error.message, 'error');
+    } else {
+      // TODO: throw new Error and move to global error handler
+      console.error(error);
+      emitToast('알 수 없는 에러가 발생했습니다. 다시 시도해주세요.', 'error');
     }
   };
 
-  const className = clsx(' rounded-full border bg-white px-2 py-1', {
-    'pointer-events-none': disabled,
-  });
+  const handleCreateReview = async () => {
+    setDisabled(true);
+
+    const data = validateAndGetData();
+
+    if (!data) {
+      return;
+    }
+
+    try {
+      const { review } = await createReview(data);
+
+      emitToast('리뷰 등록 완료', 'success');
+      router.push(`/review/${review.id}`);
+    } catch (error) {
+      handleApiError(error);
+    } finally {
+      setDisabled(false);
+    }
+  };
+
+  const handleClick = () => {
+    void handleCreateReview();
+  };
 
   return (
-    <button className={className} onClick={handleClick} aria-disabled={disabled}>
+    <button
+      role="button"
+      className={clsx('rounded-full border bg-white px-2 py-1', {
+        'pointer-events-none': disabled,
+      })}
+      onClick={handleClick}
+      aria-disabled={disabled}
+    >
       <Text>등록하기</Text>
     </button>
   );
