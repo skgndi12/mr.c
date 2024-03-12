e41a161bfe9d89b400c7f6c4b484ab95ed7698ed
wnhlee <2wheeh@gmail.com>
2024-03-12 18:21:05 +0900

    feat: implement getReview

--- ui/src/lib/apis/review/server.ts
+++ ui/src/lib/apis/review/server.ts
@@ -2,7 +2,11 @@
 
 import { unstable_noStore as noStore } from 'next/cache';
 
-import type { ListReviewsQuery, ListReviewsResponse } from '@/lib/definitions/review';
+import type {
+  GetReviewResponse,
+  ListReviewsQuery,
+  ListReviewsResponse,
+} from '@/lib/definitions/review';
 import dummyReviewList from '@/lib/dummy/review';
 
 export async function listReviews(query: ListReviewsQuery): Promise<ListReviewsResponse> {
@@ -33,5 +37,26 @@ export async function listReviews(query: ListReviewsQuery): Promise<ListReviewsR
 
     // TODO: DELETE ME when reviews api works
     return await new Promise((resolve) => setTimeout(() => resolve(dummyReviewList), 1000));
+  }
+}
+
+export async function getReview(id: string): Promise<GetReviewResponse> {
+  noStore();
+
+  try {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/reviews/${id}`);
+
+    if (!response.ok) {
+      throw new Error(
+        `Network Error: Failed to fetch review from BACKEND API: ${response.statusText}`
+      );
+    }
+
+    return (await response.json()) as GetReviewResponse;
+  } catch (error) {
+    // TODO: DELETE ME when reviews api works
+    return await new Promise((resolve) =>
+      setTimeout(() => resolve({ review: dummyReviewList.reviews[Number(id) - 1] }), 1000)
+    );
   }
 }
