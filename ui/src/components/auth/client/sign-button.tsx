f20a6e7dd72f74f20e07203ac9e721213f220d25
wnhlee <2wheeh@gmail.com>
2024-03-12 16:15:29 +0900

    feat: add loading state on auth

--- ui/src/components/auth/client/sign-button.tsx
+++ ui/src/components/auth/client/sign-button.tsx
@@ -4,28 +4,28 @@ import Text from '@/components/common/server/text';
 import { useAuth } from '@/context/auth/auth-context';
 
 export default function SignButton() {
-  const { user, signOut, signIn } = useAuth();
+  const { user, signOut, signIn, loading } = useAuth();
 
-  if (user) {
-    return (
-      <button
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        onClick={async () => {
-          await signOut();
-        }}
-      >
-        <Text size="lg" weight="medium">
-          Sign Out
-        </Text>
-      </button>
-    );
-  }
+  const state = loading ? 'loading' : user ? 'signed-in' : 'signed-out';
 
   return (
-    <button onClick={() => signIn()}>
-      <Text size="lg" weight="medium">
-        Sign In
-      </Text>
-    </button>
+    // TODO: better UI for loading state
+    <div className="w-20">
+      {state === 'loading' && null}
+      {state === 'signed-in' && (
+        <button onClick={() => void signOut()}>
+          <Text size="lg" weight="medium">
+            Sign Out
+          </Text>
+        </button>
+      )}
+      {state === 'signed-out' && (
+        <button onClick={() => signIn()}>
+          <Text size="lg" weight="medium">
+            Sign In
+          </Text>
+        </button>
+      )}
+    </div>
   );
 }
