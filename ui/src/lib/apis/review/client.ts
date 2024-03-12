c97734e88e50a511a5a516c7d36b5bf56c4f51c2
wnhlee <2wheeh@gmail.com>
2024-03-12 19:01:34 +0900

    feat: implement updateReview

--- ui/src/lib/apis/review/client.ts
+++ ui/src/lib/apis/review/client.ts
@@ -1,7 +1,12 @@
 'use client';
 
 import type { HttpErrorResponse } from '@/lib/definitions/error';
-import type { CreateReviewRequest, CreateReviewResponse } from '@/lib/definitions/review';
+import type {
+  CreateReviewRequest,
+  CreateReviewResponse,
+  UpdateReviewRequest,
+  UpdateReviewResponse,
+} from '@/lib/definitions/review';
 import dummyReviewList from '@/lib/dummy/review';
 
 export async function createReview(data: CreateReviewRequest): Promise<CreateReviewResponse> {
@@ -27,6 +32,36 @@ export async function createReview(data: CreateReviewRequest): Promise<CreateRev
     // TODO: DELETE THIS WHEN API IS READY
     return await new Promise((resolve) =>
       setTimeout(() => resolve({ review: dummyReviewList.reviews[0] }), 500)
+    );
+  }
+}
+
+export async function updateReview(
+  id: number,
+  data: UpdateReviewRequest
+): Promise<UpdateReviewResponse> {
+  try {
+    const response = await fetch(`/api/v1/reviews/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      const { messages } = (await response.json()) as HttpErrorResponse;
+
+      // TODO: build proper error message
+      console.error(
+        `status: ${response.status}\nstatusText: ${response.statusText}\nmessages:\n${messages.join('\n')}`
+      );
+
+      throw new Error('네트워크 오류가 발생했습니다. 다시 시도해주세요.');
+    }
+
+    return (await response.json()) as UpdateReviewResponse;
+  } catch (error) {
+    // TODO: DELETE THIS WHEN API IS READY
+    return await new Promise((resolve) =>
+      setTimeout(() => resolve({ review: dummyReviewList.reviews[Number(id) - 1] }), 500)
     );
   }
 }
